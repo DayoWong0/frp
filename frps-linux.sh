e55d40c1e560a60a1a8fca70723a524da8ea5040
Dayo <1690328478@qq.com>
2020-05-31 07:22:04 +0800

    Update frps-linux.sh

--- frps-linux.sh
+++ frps-linux.sh
@@ -2,5 +2,6 @@ wget https://github.com/fatedier/frp/releases/download/v0.31.2/frp_0.31.2_linux_
 tar -zxvf frp_0.31.2_linux_amd64.tar.gz&&
 cd frp_0.31.2_linux_amd64&&
 chmod +x frps&&
+echo "start run frp"&&
 nohup ./frps -c ./frps.ini&
-echo "运行成功"
+echo "stop frp"
