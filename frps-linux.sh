c0987c0d3a390b9fe369caeb44d9dc4a94efb582
Dayo <1690328478@qq.com>
2020-05-31 06:36:05 +0800

    Create frps-linux.sh

---
+++ frps-linux.sh
@@ -0,0 +1,5 @@
+wget https://github.com/fatedier/frp/releases/download/v0.31.2/frp_0.31.2_linux_amd64.tar.gz&&
+cd frp_0.31.2_linux_amd64&&
+chmod +x frps&&
+nohup ./frps -c ./frps.ini&
+echo "运行成功"
